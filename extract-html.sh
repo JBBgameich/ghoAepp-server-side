43b4b4fc9d96bc4ecac4d596d22ff360d57b2eba
JBBgameich <jbbgameich@gmail.com>
2017-12-12 19:33:17 +0100

    extract-html.sh: small cleanup

--- extract-html.sh
+++ extract-html.sh
@@ -4,23 +4,24 @@
 . ./html_template.sh
 
 export PDF_FILE_NAME="vertretungsplan.pdf"
+export HTML_FILE_NAME="vertretungsplan-table.html"
 
 # extract table from pdf
-pdf-table-extract -i vertretungsplan.pdf -t table_html -p 2 -o vertretungsplan-table.html
+pdf-table-extract -i $PDF_FILE_NAME -t table_html -p 2 -o $HTML_FILE_NAME
 
 # Replace some strings to be easier to understand
-sed -i s/"VLehrer Kürzel"/"Vertretungslehrer (Kürzel)"/g vertretungsplan-table.html
-sed -i s/"Pos"/"Stunde"/g vertretungsplan-table.html
+sed -i s/"VLehrer Kürzel"/"Vertretungslehrer (Kürzel)"/g $HTML_FILE_NAME
+sed -i s/"Pos"/"Stunde"/g $HTML_FILE_NAME
 
 # Correct spelling misstakes
-sed -i s/"Fällt"/"fällt"/g vertretungsplan-table.html
+sed -i s/"Fällt"/"fällt"/g $HTML_FILE_NAME
 
 # Vertretungsplan
-export HTML=$(cat vertretungsplan-table.html)
+export HTML=$(cat $HTML_FILE_NAME)
 export GENTIME=$(TZ='Europe/Berlin' date)
 export TITLE=Vertretungsplan
 export TARGET=vertretungsplan.html
 export SOURCE=https://gho.berlin/wp-content/frei_stunden/VPS.pdf
 gen_from_html_template
 
-rm vertretungsplan-table.html
+rm $HTML_FILE_NAME
